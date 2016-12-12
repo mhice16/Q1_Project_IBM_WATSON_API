2a803a4c5fba3a4167bf3d3bc2bd8408c208a21f
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2016-12-11 19:20:28 -0700

    Added Materialize form and information section to index.  Added form button click listener to main.js.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+  // Listen for the user to click on the data submit button.
+  // When clicked, run the analyzeIt function using the text
+  // in the text entry field.
+  $("#btn1").click(function(){
+    event.preventDefault();
+    var sendText = $("#exemplar").val();
+    //analyzeIt(sendText);  //Function that will hit Watson API.
+    //$("#exemplar").val("");  //To clear the textarea.
+  })
 });
